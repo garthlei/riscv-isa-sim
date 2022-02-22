9d54c78e9d6ede1bb3af690a3bd57b9311dc30ce
Bohan Lei <garthlei@pku.edu.cn>
2022-02-22 13:43:50 +0800

    feat: loosen stack size restriction

--- riscv/insns/pushsig.h
+++ riscv/insns/pushsig.h
@@ -1,9 +1,3 @@
-#define CFC_STACKSIZE 8
-
-if (STACK_CFC.size() < CFC_STACKSIZE) {
-  STACK_CFC.push(std::make_pair(REG_G, REG_D));
-  REG_G = 0;
-  REG_D = 0;
-} else {
-  throw trap_control_flow_fault(insn.bits());
-}
+STACK_CFC.push(std::make_pair(REG_G, REG_D));
+REG_G = 0;
+REG_D = 0;
